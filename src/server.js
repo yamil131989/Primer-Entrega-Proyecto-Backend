85ae36cdd5875039050d74810904c8e043df8d04
yamil131989 <152355713+yamil131989@users.noreply.github.com>
2024-08-01 23:49:05 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,9 @@
 const express       = require('express')
-const userRouter    = require('./routes/users.router.js')
 const productRouter = require('./routes/products.router.js')
 const logger        = require('morgan')
-const { uploader } = require('./utils/multer.js')
-
 const cartRouter = require('./routes/carts.router.js')
+
+
 // import express from 'express'
 
 const app = express()
@@ -16,17 +15,11 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/static', express.static(__dirname + '/public'))
 app.use(logger('dev'))
 
-app.use(function(req, res, next ){
-    console.log('Time: ', Date.now())
-    next()
-})
+
 
 // endpoint
 
-app.post('/', uploader.single('myFile'), (req, res)=>{
-    res.send('archivo subido')
-})
-app.use('/api/users', userRouter)
+
 app.use('/api/products', productRouter)
 app.use('/api/carts',cartRouter)
 
